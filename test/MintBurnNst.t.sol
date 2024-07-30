2437fa398d6d77f7b584d14a88e35f3a8d54293a
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-07-30 10:18:37 -0400

    feat: add allocator

---
+++ test/MintBurnNst.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.13;
+
+import { AllocatorBuffer } from "lib/dss-allocator/src/AllocatorBuffer.sol";
+import { AllocatorRoles }  from "lib/dss-allocator/src/AllocatorRoles.sol";
+import { AllocatorVault }  from "lib/dss-allocator/src/AllocatorVault.sol";
+
+import "./UnitTestBase.t.sol";
+
+contract L1ControllerACLTests is UnitTestBase {
+
+    AllocatorBuffer buffer;
+    AllocatorRoles  roles;
+    AllocatorVault  vault;
+
+    bytes32 ilk = "ilk";
+
+    function setUp() public override {
+        super.setUp();
+
+        buffer = new AllocatorBuffer();
+        roles  = new AllocatorRoles();
+        vault  = new AllocatorVault(address(roles), address(buffer));
+
+    }
+}
