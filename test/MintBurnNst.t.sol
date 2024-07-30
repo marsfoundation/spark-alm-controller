a923f35c440fa50d53929cca49f5270437354e18
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-07-30 10:20:24 -0400

    feat: add nst

--- test/MintBurnNst.t.sol
+++ test/MintBurnNst.t.sol
@@ -5,6 +5,10 @@ import { AllocatorBuffer } from "lib/dss-allocator/src/AllocatorBuffer.sol";
 import { AllocatorRoles }  from "lib/dss-allocator/src/AllocatorRoles.sol";
 import { AllocatorVault }  from "lib/dss-allocator/src/AllocatorVault.sol";
 
+import { VatMock } from "lib/dss-allocator/test/mocks/VatMock.sol";
+
+import { NstJoin } from "lib/nst/src/NstJoin.sol";
+
 import "./UnitTestBase.t.sol";
 
 contract L1ControllerACLTests is UnitTestBase {
@@ -12,6 +16,10 @@ contract L1ControllerACLTests is UnitTestBase {
     AllocatorBuffer buffer;
     AllocatorRoles  roles;
     AllocatorVault  vault;
+    NstJoin         nstJoin;
+
+    VatMock vat;
+
 
     bytes32 ilk = "ilk";
 
