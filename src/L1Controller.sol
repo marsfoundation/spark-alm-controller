d823381a5761592b3d29a908e9058984ff53c3e5
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-07-31 16:42:21 -0400

    feat: add remappings

--- src/L1Controller.sol
+++ src/L1Controller.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.13;
 
-import { AccessControl } from "lib/openzeppelin-contracts/contracts/access/AccessControl.sol";
+import { AccessControl } from "openzeppelin-contracts/contracts/access/AccessControl.sol";
 
-import { UpgradeableProxied } from "lib/upgradeable-proxy/src/UpgradeableProxied.sol";
+import { UpgradeableProxied } from "upgradeable-proxy/UpgradeableProxied.sol";
 
 contract L1Controller is UpgradeableProxied, AccessControl {
 
