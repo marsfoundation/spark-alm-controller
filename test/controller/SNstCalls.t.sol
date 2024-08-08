6e5f2074330fa42d4a5d24485260dc5f9b7be79e
Lucas Manuel <lucasmanuel.tech@gmail.com>
2024-08-08 10:02:44 -0400

    feat: Add sNST withdraw functionality (SC-566) (#6)

--- test/controller/SNstCalls.t.sol
+++ test/controller/SNstCalls.t.sol
@@ -60,3 +60,63 @@ contract EthereumControllerSwapNSTToSNSTTests is UnitTestBase {
 
 }
 
+contract EthereumControllerSwapSNSTToNSTFailureTests is UnitTestBase {
+
+    function test_swapSNSTToNST_notRelayer() external {
+        vm.expectRevert(abi.encodeWithSignature(
+            "AccessControlUnauthorizedAccount(address,bytes32)",
+            address(this),
+            RELAYER
+        ));
+        ethereumController.swapSNSTToNST(1e18);
+    }
+
+    function test_swapSNSTToNST_frozen() external {
+        vm.prank(freezer);
+        ethereumController.freeze();
+
+        vm.prank(relayer);
+        vm.expectRevert("EthereumController/not-active");
+        ethereumController.swapSNSTToNST(1e18);
+    }
+
+}
+
+contract EthereumControllerSwapSNSTToNSTTests is UnitTestBase {
+
+    function test_swapSNSTToNST() external {
+        vm.startPrank(relayer);
+        ethereumController.mintNST(1e18);
+        ethereumController.swapNSTToSNST(1e18);
+        vm.stopPrank();
+
+        assertEq(nst.balanceOf(address(almProxy)),           0);
+        assertEq(nst.balanceOf(address(ethereumController)), 0);
+        assertEq(nst.balanceOf(address(snst)),               1e18);
+
+        assertEq(nst.allowance(address(buffer),   address(vault)), type(uint256).max);
+        assertEq(nst.allowance(address(almProxy), address(snst)),  0);
+
+        // NOTE: 1:1 exchange rate
+        assertEq(snst.totalSupply(),                1e18);
+        assertEq(snst.totalAssets(),                1e18);
+        assertEq(snst.balanceOf(address(almProxy)), 1e18);
+
+        vm.prank(relayer);
+        ethereumController.swapSNSTToNST(1e18);
+
+        assertEq(nst.balanceOf(address(almProxy)),           1e18);
+        assertEq(nst.balanceOf(address(ethereumController)), 0);
+        assertEq(nst.balanceOf(address(snst)),               0);
+
+        assertEq(nst.allowance(address(buffer),   address(vault)), type(uint256).max);
+        assertEq(nst.allowance(address(almProxy), address(snst)),  0);
+
+        assertEq(snst.totalSupply(),                0);
+        assertEq(snst.totalAssets(),                0);
+        assertEq(snst.balanceOf(address(almProxy)), 0);
+    }
+
+}
+
+
