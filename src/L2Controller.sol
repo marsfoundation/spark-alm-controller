34f4eb67783bc20aee8bbdcb7edb9993ef2dbfa7
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-08-16 12:05:02 -0400

    feat: start structure

---
+++ src/L2Controller.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.21;
+
+import { IERC20 }   from "forge-std/interfaces/IERC20.sol";
+
+import { AccessControl } from "openzeppelin-contracts/contracts/access/AccessControl.sol";
+
+import { IALMProxy } from "src/interfaces/IALMProxy.sol";
+
+contract L2Controller is AccessControl {
+
+}
