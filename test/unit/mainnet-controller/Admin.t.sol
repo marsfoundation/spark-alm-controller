57a94d041ad455a0db2db2993f694072f59db1f9
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-08-20 09:54:57 -0400

    feat: add setting capability

---
+++ test/unit/mainnet-controller/Admin.t.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.21;
+
+import "test/unit/UnitTestBase.t.sol";
+
+contract MainnetControllerFreezeTests is UnitTestBase {
+
+    bytes32 mintRecipient1 = bytes32(uint256(uint160(makeAddr("mintRecipient1"))));
+    bytes32 mintRecipient2 = bytes32(uint256(uint160(makeAddr("mintRecipient2"))));
+
+    function test_setMintRecipient_unauthorizedAccount() public {
+        vm.expectRevert(abi.encodeWithSignature(
+            "AccessControlUnauthorizedAccount(address,bytes32)",
+            address(this),
+            DEFAULT_ADMIN_ROLE
+        ));
+        mainnetController.setMintRecipient(1, mintRecipient1);
+
+        vm.prank(freezer);
+        vm.expectRevert(abi.encodeWithSignature(
+            "AccessControlUnauthorizedAccount(address,bytes32)",
+            freezer,
+            DEFAULT_ADMIN_ROLE
+        ));
+        mainnetController.setMintRecipient(1, mintRecipient1);
+    }
+
+    function test_setMintRecipient() public {
+        assertEq(mainnetController.mintRecipients(1), bytes32(0));
+        assertEq(mainnetController.mintRecipients(2), bytes32(0));
+
+        vm.prank(admin);
+        mainnetController.setMintRecipient(1, mintRecipient1);
+
+        assertEq(mainnetController.mintRecipients(1), mintRecipient1);
+
+        vm.prank(admin);
+        mainnetController.setMintRecipient(2, mintRecipient2);
+
+        assertEq(mainnetController.mintRecipients(2), mintRecipient2);
+
+        vm.prank(admin);
+        mainnetController.setMintRecipient(1, mintRecipient2);
+
+        assertEq(mainnetController.mintRecipients(1), mintRecipient2);
+    }
+
+}
