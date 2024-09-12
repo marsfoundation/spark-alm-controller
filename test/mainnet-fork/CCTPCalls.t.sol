85c2ec8d5c63956e79ea14ee599e330b80ff0fe8
Sam MacPherson <xylom44@gmail.com>
2024-09-13 06:25:56 +0800

    put redundant emits in an internal function

--- test/mainnet-fork/CCTPCalls.t.sol
+++ test/mainnet-fork/CCTPCalls.t.sol
@@ -219,18 +219,9 @@ contract USDCToCCTPIntegrationTests is BaseChainUSDCToCCTPTestBase {
 
         assertEq(usds.allowance(address(almProxy), CCTP_MESSENGER),  0);
 
-        // NOTE: Focusing on burnToken, amount, depositor, mintRecipient, and destinationDomain
-        //       for assertions
-        vm.expectEmit(CCTP_MESSENGER);
-        emit DepositForBurn(
+        _expectEthereumCCTPEmit(
             94773,
-            address(usdc),
-            1e6,
-            address(almProxy),
-            mainnetController.mintRecipients(CCTPForwarder.DOMAIN_ID_CIRCLE_BASE),
-            CCTPForwarder.DOMAIN_ID_CIRCLE_BASE,
-            bytes32(0x0000000000000000000000001682ae6375c4e4a97e4b583bc394c861a46d8962),
-            bytes32(0x0000000000000000000000000000000000000000000000000000000000000000)
+            1e6
         );
 
         vm.prank(relayer);
@@ -265,27 +256,13 @@ contract USDCToCCTPIntegrationTests is BaseChainUSDCToCCTPTestBase {
         assertEq(usds.allowance(address(almProxy), CCTP_MESSENGER),  0);
 
         // Will split into two separate transactions at max 1m each
-        vm.expectEmit(CCTP_MESSENGER);
-        emit DepositForBurn(
+        _expectEthereumCCTPEmit(
             94773,
-            address(usdc),
-            1_000_000e6,
-            address(almProxy),
-            mainnetController.mintRecipients(CCTPForwarder.DOMAIN_ID_CIRCLE_BASE),
-            CCTPForwarder.DOMAIN_ID_CIRCLE_BASE,
-            bytes32(0x0000000000000000000000001682ae6375c4e4a97e4b583bc394c861a46d8962),
-            bytes32(0x0000000000000000000000000000000000000000000000000000000000000000)
+            1_000_000e6
         );
-        vm.expectEmit(CCTP_MESSENGER);
-        emit DepositForBurn(
+        _expectEthereumCCTPEmit(
             94774,
-            address(usdc),
-            900_000e6,
-            address(almProxy),
-            mainnetController.mintRecipients(CCTPForwarder.DOMAIN_ID_CIRCLE_BASE),
-            CCTPForwarder.DOMAIN_ID_CIRCLE_BASE,
-            bytes32(0x0000000000000000000000001682ae6375c4e4a97e4b583bc394c861a46d8962),
-            bytes32(0x0000000000000000000000000000000000000000000000000000000000000000)
+            900_000e6
         );
 
         vm.prank(relayer);
@@ -321,18 +298,9 @@ contract USDCToCCTPIntegrationTests is BaseChainUSDCToCCTPTestBase {
 
         assertEq(usdsBase.allowance(address(foreignAlmProxy), CCTP_MESSENGER_BASE),  0);
 
-        // NOTE: Focusing on burnToken, amount, depositor, mintRecipient, and destinationDomain
-        //       for assertions
-        vm.expectEmit(CCTP_MESSENGER_BASE);
-        emit DepositForBurn(
+        _expectBaseCCTPEmit(
             255141,
-            address(usdcBase),
-            1e6,
-            address(foreignAlmProxy),
-            foreignController.mintRecipients(CCTPForwarder.DOMAIN_ID_CIRCLE_ETHEREUM),
-            CCTPForwarder.DOMAIN_ID_CIRCLE_ETHEREUM,
-            bytes32(0x000000000000000000000000bd3fa81b58ba92a82136038b25adec7066af3155),
-            bytes32(0x0000000000000000000000000000000000000000000000000000000000000000)
+            1e6
         );
 
         vm.prank(relayer);
@@ -369,38 +337,17 @@ contract USDCToCCTPIntegrationTests is BaseChainUSDCToCCTPTestBase {
         assertEq(usdsBase.allowance(address(foreignAlmProxy), CCTP_MESSENGER_BASE),  0);
 
         // Will split into three separate transactions at max 1m each
-        vm.expectEmit(CCTP_MESSENGER_BASE);
-        emit DepositForBurn(
+        _expectBaseCCTPEmit(
             255141,
-            address(usdcBase),
-            1_000_000e6,
-            address(foreignAlmProxy),
-            foreignController.mintRecipients(CCTPForwarder.DOMAIN_ID_CIRCLE_ETHEREUM),
-            CCTPForwarder.DOMAIN_ID_CIRCLE_ETHEREUM,
-            bytes32(0x000000000000000000000000bd3fa81b58ba92a82136038b25adec7066af3155),
-            bytes32(0x0000000000000000000000000000000000000000000000000000000000000000)
+            1_000_000e6
         );
-        vm.expectEmit(CCTP_MESSENGER_BASE);
-        emit DepositForBurn(
+        _expectBaseCCTPEmit(
             255142,
-            address(usdcBase),
-            1_000_000e6,
-            address(foreignAlmProxy),
-            foreignController.mintRecipients(CCTPForwarder.DOMAIN_ID_CIRCLE_ETHEREUM),
-            CCTPForwarder.DOMAIN_ID_CIRCLE_ETHEREUM,
-            bytes32(0x000000000000000000000000bd3fa81b58ba92a82136038b25adec7066af3155),
-            bytes32(0x0000000000000000000000000000000000000000000000000000000000000000)
+            1_000_000e6
         );
-        vm.expectEmit(CCTP_MESSENGER_BASE);
-        emit DepositForBurn(
+        _expectBaseCCTPEmit(
             255143,
-            address(usdcBase),
-            600_000e6,
-            address(foreignAlmProxy),
-            foreignController.mintRecipients(CCTPForwarder.DOMAIN_ID_CIRCLE_ETHEREUM),
-            CCTPForwarder.DOMAIN_ID_CIRCLE_ETHEREUM,
-            bytes32(0x000000000000000000000000bd3fa81b58ba92a82136038b25adec7066af3155),
-            bytes32(0x0000000000000000000000000000000000000000000000000000000000000000)
+            600_000e6
         );
 
         vm.prank(relayer);
@@ -423,6 +370,38 @@ contract USDCToCCTPIntegrationTests is BaseChainUSDCToCCTPTestBase {
         assertEq(usdc.balanceOf(address(almProxy)),          2_600_000e6);
         assertEq(usdc.balanceOf(address(mainnetController)), 0);
         assertEq(usdc.totalSupply(),                         USDC_SUPPLY + 2_600_000e6);
+    }
+
+    function _expectEthereumCCTPEmit(uint64 nonce, uint256 amount) internal {
+        // NOTE: Focusing on burnToken, amount, depositor, mintRecipient, and destinationDomain
+        //       for assertions
+        vm.expectEmit(CCTP_MESSENGER);
+        emit DepositForBurn(
+            nonce,
+            address(usdc),
+            amount,
+            address(almProxy),
+            mainnetController.mintRecipients(CCTPForwarder.DOMAIN_ID_CIRCLE_BASE),
+            CCTPForwarder.DOMAIN_ID_CIRCLE_BASE,
+            bytes32(0x0000000000000000000000001682ae6375c4e4a97e4b583bc394c861a46d8962),
+            bytes32(0x0000000000000000000000000000000000000000000000000000000000000000)
+        );
+    }
+
+    function _expectBaseCCTPEmit(uint64 nonce, uint256 amount) internal {
+        // NOTE: Focusing on burnToken, amount, depositor, mintRecipient, and destinationDomain
+        //       for assertions
+        vm.expectEmit(CCTP_MESSENGER_BASE);
+        emit DepositForBurn(
+            nonce,
+            address(usdcBase),
+            amount,
+            address(foreignAlmProxy),
+            foreignController.mintRecipients(CCTPForwarder.DOMAIN_ID_CIRCLE_ETHEREUM),
+            CCTPForwarder.DOMAIN_ID_CIRCLE_ETHEREUM,
+            bytes32(0x000000000000000000000000bd3fa81b58ba92a82136038b25adec7066af3155),
+            bytes32(0x0000000000000000000000000000000000000000000000000000000000000000)
+        );
     }
 
 }
