6415e7edf2a8d58ffd4fe38934b6dbc64f4c7f5b
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-09-25 09:02:35 -0400

    feat: fix all tests

--- test/mainnet-fork/PsmCalls.t.sol
+++ test/mainnet-fork/PsmCalls.t.sol
@@ -127,8 +127,63 @@ contract MainnetControllerSwapUSDCToUSDSFailureTests is ForkTestBase {
         mainnetController.swapUSDCToUSDS(1e6);
     }
 
-    function test_swapUSDCToUSDS_firstRefillIncomplete() external {}
-    function test_swapUSDCToUSDS_secondfRefillIncomplete() external {}
+    function test_swapUSDCToUSDS_incompleteFillBoundary() external {
+        assertEq(DAI_BAL_PSM, 204_506_488.11013e18);
+
+        DssLitePsm psmCode = new DssLitePsm(psm.ilk(), address(psm.gem()), address(psm.daiJoin()), psm.pocket());
+
+        vm.etch(PSM, address(psmCode).code);
+
+        uint256 fillAmount = psm.rush();
+
+        assertEq(fillAmount, 0);
+
+        // The line is just over 2.1 billion, this condition will allow DAI to get minted to get to
+        // 2 billion in Art, and then another fill to get to the `line`.
+        deal(address(usdc), address(pocket), 1_800_000_000e6);
+
+        fillAmount = psm.rush();
+
+        assertEq(fillAmount, 121_680_037.47418e18);  // Only first fill amount
+
+        // NOTE: Art == dai here because rate is 1 for PSM ilk
+        ( uint256 Art,,, uint256 line, ) = dss.vat.ilks(PSM_ILK);
+
+        assertEq(Art,  1_878_319_962.52582e18);
+        assertEq(Art,  2_000_000_000e18 - fillAmount);  // First fill gets art to 2 billion
+        assertEq(line, 2_124_094_563.678406e45);
+
+        // The first fill increases the Art to 2 billion and the USDC balance of the PSM to 2.1 billion.
+        // For the second fill, the USDC balance + buffer option is ~2.3 billion so it instead fills to the line
+        // which is 2.124 billion.
+        uint256 expectedFillAmount2 = line / 1e27 - 2_000_000_000e18;
+
+        assertEq(expectedFillAmount2, 124_094_563.678406e18);
+
+        // Max amount of DAI that can be swapped, converted to USDC precision
+        uint256 maxSwapAmount = (DAI_BAL_PSM + fillAmount + expectedFillAmount2) / 1e12;
+
+        assertEq(maxSwapAmount, 450_281_089.262716e6);
+
+        deal(address(usdc), address(almProxy), maxSwapAmount + 1);  // Higher than balance of DAI + fillAmount
+
+        // assertEq(Art + fillAmount + expectedFillAmount2, line / 1e27);  // Two fills will increase Art to the debt ceiling
+
+        vm.startPrank(relayer);
+        vm.expectRevert("DssLitePsm/nothing-to-fill");
+        mainnetController.swapUSDCToUSDS(maxSwapAmount + 1);
+
+        mainnetController.swapUSDCToUSDS(maxSwapAmount);
+
+        ( Art,,,, ) = dss.vat.ilks(PSM_ILK);
+
+        // Art has now been filled to the debt ceiling and there is no DAI left in the PSM.
+        assertEq(Art, line / 1e27);
+        assertEq(Art, 2_124_094_563.678406e18);
+
+        assertEq(dai.balanceOf(address(PSM)), 0);
+    }
+
 }
 
 contract MainnetControllerSwapUSDCToUSDSTests is ForkTestBase {
@@ -264,7 +319,9 @@ contract MainnetControllerSwapUSDCToUSDSTests is ForkTestBase {
 
         fillAmount = psm.rush();
 
-        deal(address(usdc), address(almProxy), 400_000_000e6);  // Higher than balance of DAI
+        assertEq(fillAmount, 121_680_037.47418e18);  // Only first fill amount
+
+        deal(address(usdc), address(almProxy), 400_000_000e6);  // Higher than balance of DAI + fillAmount
 
         assertEq(usds.balanceOf(address(almProxy)),          0);
         assertEq(usds.balanceOf(address(mainnetController)), 0);
@@ -286,7 +343,7 @@ contract MainnetControllerSwapUSDCToUSDSTests is ForkTestBase {
         ( uint256 Art,,, uint256 line, ) = dss.vat.ilks(PSM_ILK);
 
         assertEq(Art,  1_878_319_962.52582e18);
-        assertEq(Art,  2_000_000_000e18 - fillAmount);
+        assertEq(Art,  2_000_000_000e18 - fillAmount);  // First fill gets art to 2 billion
         assertEq(line, 2_124_094_563.678406e45);
 
         // The first fill increases the Art to 2 billion and the USDC balance of the PSM to 2.1 billion.
@@ -294,8 +351,9 @@ contract MainnetControllerSwapUSDCToUSDSTests is ForkTestBase {
         // which is 2.124 billion.
         uint256 expectedFillAmount2 = line / 1e27 - 2_000_000_000e18;
 
-        assertEq(fillAmount,          121_680_037.47418e18);  // Only first fill amount
         assertEq(expectedFillAmount2, 124_094_563.678406e18);
+
+        assertEq(Art + fillAmount + expectedFillAmount2, line / 1e27);  // Two fills will increase Art to the debt ceiling
 
         vm.prank(relayer);
         vm.expectEmit(PSM);
