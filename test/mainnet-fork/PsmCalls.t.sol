38a77764250d213f95f471245a7fb98ce96c8f30
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-09-26 07:55:25 -0400

    fix: prank relayer

--- test/mainnet-fork/PsmCalls.t.sol
+++ test/mainnet-fork/PsmCalls.t.sol
@@ -417,12 +417,12 @@ contract MainnetControllerSwapUSDCToUSDSTests is ForkTestBase {
         uint256 usdsBalanceBefore = usds.balanceOf(address(almProxy));
 
         // NOTE: Doing a low-level call here because if the full amount can't be swapped, it should revert
+        vm.prank(relayer);
         ( bool success, ) = address(mainnetController).call(
             abi.encodeWithSignature("swapUSDCToUSDS(uint256)", swapAmount)
         );
 
         if (success) {
-            assertTrue(false);
             assertEq(usds.balanceOf(address(almProxy)), usdsBalanceBefore + swapAmount * 1e12);
         }
     }
