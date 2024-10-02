797be93f5937645b5f5594c28c6c7078f02b8f81
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-10-02 10:00:52 -0400

    feat: add more script logic

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,104 +3,57 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
 
+import { Base } from "lib/spark-address-registry/src/Base.sol";
+
+import { Ethereum } from "lib/spark-address-registry/src/Ethereum.sol";
+
 import { ControllerInstance } from "../deploy/ControllerInstance.sol";
 
 import { ForeignControllerDeploy, MainnetControllerDeploy } from "../deploy/ControllerDeploy.sol";
 
-contract DeployMainnetAddresses is Script {
-
-    address constant CCTP_MESSENGER = 0xBd3fa81B58Ba92a82136038B25aDec7066af3155;
-    address constant PSM            = 0xf6e72Db5454dd049d0788e411b06CfAF16853042;
-    address constant SPARK_PROXY    = 0x3300f198988e4C9C63F75dF86De36421f06af8c4;
-
-    function deployFull(string memory remoteRpcUrl) internal {
-        vm.startBroadcast();
-
-        ControllerInstance memory instance = ForeignControllerDeploy.deployFull({
-            admin      : admin,
-            almProxy   : almProxy,
-            rateLimits : rateLimits,
-            psm        : psm,
-            usdc       : usdc,
-            cctp       : cctp
-        });
-
-        vm.stopBroadcast();
-    }
-
-}
-
-contract DeployMainnetFull is DeployMainnetAddresses, Script {
+contract DeployMainnetFull is Script {
 
     function run() internal {
+
+        console.log("Deploying Mainnet ALMProxy, Controller and RateLimits...");
+
         vm.startBroadcast();
 
-        ControllerInstance memory instance = ForeignControllerDeploy.deployFull({
-            admin      : admin,
-            almProxy   : almProxy,
-            rateLimits : rateLimits,
-            psm        : psm,
-            usdc       : usdc,
-            cctp       : cctp
+        ControllerInstance memory instance = MainnetControllerDeploy.deployFull({
+            admin      : Ethereum.SPARK_PROXY,
+            psm        : Ethereum.PSM,
+            usdc       : Ethereum.USDC,
+            cctp       : Ethereum.  //  TODO: xchain-helpers forwarders
         });
 
         vm.stopBroadcast();
+
+        console.log("ALMProxy   deployed at", instance.almProxy);
+        console.log("Controller deployed at", instance.controller);
+        console.log("RateLimits deployed at", instance.rateLimits);
     }
 
 }
 
-contract DeployBase is Deploy {
+contract DeployBaseFull is Script {
 
-    function run() external {
-        deploy(getChain("base").rpcUrl);
-    }
+    function run() internal {
+        console.log("Deploying Mainnet ALMProxy, Controller and RateLimits...");
 
-}
+        vm.startBroadcast();
 
-contract DeployWorldChain is Deploy {
+        ControllerInstance memory instance = ForeignControllerDeploy.deployFull({
+            admin      : Ethereum.SPARK_PROXY,
+            psm        : Ethereum.PSM,
+            usdc       : Ethereum.USDC,
+            cctp       : Ethereum.CCTP_MESSENGER
+        });
 
-    function run() external {
-        deploy(vm.envString("WORLD_CHAIN_RPC_URL"));
-    }
+        vm.stopBroadcast();
 
-    function deployForwarder(address receiver) internal override returns (address) {
-        return address(new SSROracleForwarderOptimism(SUSDS, receiver, OptimismForwarder.L1_CROSS_DOMAIN_WORLD_CHAIN));
-    }
-
-    function deployReceiver(address forwarder, address oracle) internal override returns (address) {
-        return address(new OptimismReceiver(forwarder, oracle));
-    }
-
-}
-
-contract DeployGnosis is Deploy {
-
-    function run() external {
-        deploy(getChain("gnosis_chain").rpcUrl);
-    }
-
-    function deployForwarder(address receiver) internal override returns (address) {
-        return address(new SSROracleForwarderGnosis(SUSDS, receiver));
-    }
-
-    function deployReceiver(address forwarder, address oracle) internal override returns (address) {
-        return address(new AMBReceiver(Gnosis.L2_AMB, bytes32(uint256(1)), forwarder, oracle));
-    }
-
-}
-
-contract DeployArbitrumOne is Deploy {
-
-    function run() external {
-        deploy(getChain("arbitrum_one").rpcUrl);
-    }
-
-    function deployForwarder(address receiver) internal override returns (address) {
-        return address(new SSROracleForwarderArbitrum(SUSDS, receiver, ArbitrumForwarder.L1_CROSS_DOMAIN_ARBITRUM_ONE));
-    }
-
-    function deployReceiver(address forwarder, address oracle) internal override returns (address) {
-        return address(new ArbitrumReceiver(forwarder, oracle));
+        console.log("ALMProxy   deployed at", instance.almProxy);
+        console.log("Controller deployed at", instance.controller);
+        console.log("RateLimits deployed at", instance.rateLimits);
     }
 
 }
