8dbd97ec81c25387f17e9a572efcecb410171977
Sam MacPherson <xylom44@gmail.com>
2024-10-05 11:55:24 -0400

    almost finished base support

--- script/testnet/DeploySepolia.s.sol
+++ script/testnet/DeploySepolia.s.sol
@@ -4,11 +4,8 @@ pragma solidity ^0.8.21;
 import { Script, console } from "forge-std/Script.sol";
 import { IERC20 }          from "forge-std/interfaces/IERC20.sol";
 import { ScriptTools }     from "dss-test/ScriptTools.sol";
-import { ERC1967Proxy }    from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import { MockERC20 }       from "erc20-helpers/MockERC20.sol";
-
-import { Usds }  from "lib/usds/src/Usds.sol";
-import { SUsds } from "lib/sdai/src/SUsds.sol";
+import { CCTPForwarder }   from "xchain-helpers/src/forwarders/CCTPForwarder.sol";
 
 import {
     AllocatorDeploy,
@@ -27,19 +24,26 @@ import { AllocatorVault }  from "lib/dss-allocator/src/AllocatorVault.sol";
 import {
     MainnetControllerDeploy,
     ForeignControllerDeploy,
-    ControllerInstance
+    ControllerInstance,
+    MainnetController,
+    ForeignController
 } from "deploy/ControllerDeploy.sol";
 import {
     MainnetControllerInit,
+    ForeignControllerInit,
     RateLimitData
 } from "deploy/ControllerInit.sol";
 
-import { Jug }        from "../common/Jug.sol";
-import { PauseProxy } from "../common/PauseProxy.sol";
-import { Vat }        from "../common/Vat.sol";
-import { UsdsJoin }   from "../common/UsdsJoin.sol";
-import { DaiUsds }    from "../common/DaiUsds.sol";
-import { PSM }        from "./PSM.sol";
+import { PSM3 } from "lib/spark-psm/src/PSM3.sol";
+
+import { Jug }          from "../common/Jug.sol";
+import { PauseProxy }   from "../common/PauseProxy.sol";
+import { Vat }          from "../common/Vat.sol";
+import { UsdsJoin }     from "../common/UsdsJoin.sol";
+import { DaiUsds }      from "../common/DaiUsds.sol";
+import { PSM }          from "./PSM.sol";
+import { RateProvider } from "./RateProvider.sol";
+import { SUsds }        from "./SUsds.sol";
 
 struct Domain {
     string  name;
@@ -66,7 +70,7 @@ contract DeploySepolia is Script {
 
     // Mainnet contracts
     MockERC20 dai;
-    Usds usds;
+    MockERC20 usds;
     SUsds susds;
     IERC20 usdc = IERC20(USDC);
 
@@ -80,21 +84,14 @@ contract DeploySepolia is Script {
     AllocatorSharedInstance allocatorSharedInstance;
     AllocatorIlkInstance    allocatorIlkInstance;
 
+    ControllerInstance mainnetController;
+
     // Base contracts
-
-    function _deployUSDS(address _deployer, address _owner) internal {
-        address _usdsImp = address(new Usds());
-        address _usds = address((new ERC1967Proxy(_usdsImp, abi.encodeCall(Usds.initialize, ()))));
-        ScriptTools.switchOwner(_usds, _deployer, _owner);
-        usds = Usds(_usds);
-    }
-
-    function _deploySUSDS(address _deployer, address _owner) internal {
-        address _sUsdsImp = address(new SUsds(address(usdsJoin), address(0)));
-        address _sUsds = address(new ERC1967Proxy(_sUsdsImp, abi.encodeCall(SUsds.initialize, ())));
-        ScriptTools.switchOwner(_sUsds, _deployer, _owner);
-        susds = SUsds(_sUsds);
-    }
+    IERC20 usdcBase = IERC20(USDC);
+    MockERC20 usdsBase;
+    MockERC20 susdsBase;
+    
+    PSM3 psmBase;
 
     function setupMCDMocks() internal {
         vm.selectFork(mainnet.forkId);
@@ -107,7 +104,8 @@ contract DeploySepolia is Script {
 
         // Init tokens
         dai = new MockERC20("DAI", "DAI", 18);
-        _deployUSDS(deployer, mainnet.admin);
+        usds = new MockERC20("USDS", "USDS", 18);
+        susds = new SUsds(address(usds));
 
         // Init MCD contracts
         vat        = new Vat();
@@ -116,9 +114,6 @@ contract DeploySepolia is Script {
         daiUsds    = new DaiUsds(mainnet.admin, address(dai), address(usds));
         jug        = new Jug();
         psm        = new PSM(mainnet.admin, address(usdc), address(dai));
-
-        // Dependency on usdsJoin
-        _deploySUSDS(deployer, mainnet.admin);
 
         // Mint some USDS into the join contract
         usds.mint(address(usdsJoin), USDS_UNIT_SIZE);
@@ -179,7 +174,7 @@ contract DeploySepolia is Script {
         
         vm.startBroadcast();
 
-        ControllerInstance memory instance = MainnetControllerDeploy.deployFull({
+        ControllerInstance memory instance = mainnetController = MainnetControllerDeploy.deployFull({
             admin:   mainnet.admin,
             vault:   address(allocatorIlkInstance.vault),
             psm:     address(psm),
@@ -236,6 +231,27 @@ contract DeploySepolia is Script {
         ScriptTools.exportContract(mainnet.name, "controller", instance.controller);
         ScriptTools.exportContract(mainnet.name, "rateLimits", instance.rateLimits);
     }
+
+    function setupBasePSM() public {
+        vm.selectFork(base.forkId);
+        
+        vm.startBroadcast();
+
+        usdsBase = new MockERC20("USDS", "USDS", 18);
+        susdsBase = new MockERC20("sUSDS", "sUSDS", 18);
+
+        psmBase = new PSM3(
+            base.admin,
+            address(usdcBase),
+            address(usdsBase),
+            address(susdsBase),
+            address(new RateProvider())
+        );
+
+        vm.stopBroadcast();
+
+        ScriptTools.exportContract(base.name, "psm", address(psmBase));
+    }
     
     function setupBaseALMController() public {
         vm.selectFork(base.forkId);
@@ -258,8 +274,8 @@ contract DeploySepolia is Script {
             slope:     0
         });
 
-        ForeignControllerDeploy.init({
-            params: ForeignControllerDeploy.AddressParams({
+        ForeignControllerInit.init({
+            params: ForeignControllerInit.AddressParams({
                 admin:   base.admin,
                 freezer: makeAddr("freezer"),
                 relayer: deployer, // TODO: replace with SAFE
@@ -271,7 +287,7 @@ contract DeploySepolia is Script {
                 susds: USDC_BASE
             }),
             controllerInst: instance,
-            data: ForeignControllerDeploy.InitRateLimitData({
+            data: ForeignControllerInit.InitRateLimitData({
                 usdcDepositData: rateLimitData6,
                 usdcWithdrawData: rateLimitData6,
                 usdcToCctpData: unlimitedRateLimit,
@@ -279,15 +295,23 @@ contract DeploySepolia is Script {
             })
         });
 
-        // Custom contract permission changes (not relevant for production deploy)
-        daiUsds.transferOwnership(instance.almProxy);
-        psm.transferOwnership(instance.almProxy);
+        // FIXME - remove this when done in deployer scripts
+        ForeignController(instance.controller).setMintRecipient(CCTPForwarder.DOMAIN_ID_CIRCLE_ETHEREUM, bytes32(uint256(uint160(mainnetController.almProxy))));
 
         vm.stopBroadcast();
 
-        ScriptTools.exportContract(mainnet.name, "almProxy",   instance.almProxy);
-        ScriptTools.exportContract(mainnet.name, "controller", instance.controller);
-        ScriptTools.exportContract(mainnet.name, "rateLimits", instance.rateLimits);
+        vm.selectFork(mainnet.forkId);
+        
+        vm.startBroadcast();
+
+        // FIXME - remove this when done in deployer scripts
+        MainnetController(mainnetController.controller).setMintRecipient(CCTPForwarder.DOMAIN_ID_CIRCLE_BASE, bytes32(uint256(uint160(instance.almProxy))));
+
+        vm.stopBroadcast();
+
+        ScriptTools.exportContract(base.name, "almProxy",   instance.almProxy);
+        ScriptTools.exportContract(base.name, "controller", instance.controller);
+        ScriptTools.exportContract(base.name, "rateLimits", instance.rateLimits);
     }
 
     function run() public {
@@ -298,7 +322,8 @@ contract DeploySepolia is Script {
         ilk      = "ALLOCATOR-SPARK-1";
 
         setChain("sepolia_base", ChainData({
-            rpcUrl: "https://base-sepolia-rpc.publicnode.com",
+            //rpcUrl: "https://base-sepolia-rpc.publicnode.com",
+            rpcUrl: "http://localhost:8546",
             chainId: 84532,
             name: "Sepolia Base Testnet"
         }));
--- script/testnet/DeploySepolia.t.sol
+++ script/testnet/DeploySepolia.t.sol
@@ -8,6 +8,7 @@ import { ScriptTools } from "dss-test/ScriptTools.sol";
 import { Bridge }                from "xchain-helpers/src/testing/Bridge.sol";
 import { Domain, DomainHelpers } from "xchain-helpers/src/testing/Domain.sol";
 import { CCTPBridgeTesting }     from "xchain-helpers/src/testing/bridges/CCTPBridgeTesting.sol";
+import { CCTPForwarder }         from "xchain-helpers/src/forwarders/CCTPForwarder.sol";
 
 import { Usds }  from "lib/usds/src/Usds.sol";
 import { SUsds } from "lib/sdai/src/SUsds.sol";
@@ -26,6 +27,7 @@ contract DeploySepoliaTest is Test {
 
     using stdJson for *;
     using DomainHelpers for *;
+    using CCTPBridgeTesting for *;
 
     address CCTP_TOKEN_MESSENGER_MAINNET = 0x9f3B8679c73C2Fef8b59B4f3444d4e156fb70AA5;
     address CCTP_MESSENGER_MAINNET       = 0x7865fAfC2db2093669d92c0F33AeEF291086BEFD;
@@ -106,15 +108,24 @@ contract DeploySepoliaTest is Test {
         mainnetController = MainnetController(outputMainnet.readAddress(".controller"));
         almProxy          = ALMProxy(payable(outputMainnet.readAddress(".almProxy")));
         rateLimits        = RateLimits(outputMainnet.readAddress(".rateLimits"));
+
+        psm = PSM3(outputBase.readAddress(".psm"));
+
+        usdsBase  = IERC20(outputBase.readAddress(".usds"));
+        susdsBase = IERC20(outputBase.readAddress(".sUsds"));
+        usdcBase  = IERC20(outputBase.readAddress(".usdc"));
+
+        foreignController = ForeignController(outputBase.readAddress(".controller"));
+        almProxyBase      = ALMProxy(payable(outputBase.readAddress(".almProxy")));
     }
 
     function test_mintUSDS() public {
         assertEq(usds.balanceOf(address(almProxy)), 0);
 
         vm.prank(admin);
-        mainnetController.mintUSDS(1e18);
+        mainnetController.mintUSDS(1000e18);
 
-        assertEq(usds.balanceOf(address(almProxy)), 1e18);
+        assertEq(usds.balanceOf(address(almProxy)), 1000e18);
     }
 
     function test_mintAndSwapToUSDC() public {
@@ -122,12 +133,30 @@ contract DeploySepoliaTest is Test {
 
         vm.startPrank(admin);
 
-        mainnetController.mintUSDS(1e18);
-        mainnetController.swapUSDSToUSDC(1e6);
+        mainnetController.mintUSDS(1000e18);
+        mainnetController.swapUSDSToUSDC(1000e6);
 
         vm.stopPrank();
 
-        assertEq(usdc.balanceOf(address(almProxy)), 1e6);
+        assertEq(usdc.balanceOf(address(almProxy)), 1000e6);
+    }
+
+    function test_transferCCTP() public {
+        base.selectFork();
+
+        assertEq(usdcBase.balanceOf(address(almProxyBase)), 0);
+
+        vm.startPrank(admin);
+
+        mainnetController.mintUSDS(1000e18);
+        mainnetController.swapUSDSToUSDC(1000e6);
+        mainnetController.transferUSDCToCCTP(CCTPForwarder.DOMAIN_ID_CIRCLE_BASE, 1000e6);
+
+        vm.stopPrank();
+
+        cctpBridge.relayMessagesToDestination(true);
+
+        assertEq(usdcBase.balanceOf(address(almProxyBase)), 1000e6);
     }
 
 }
