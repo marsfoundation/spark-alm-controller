1885ef54d25f20b1fccf7dff26a8949749b7e618
Sam MacPherson <xylom44@gmail.com>
2024-10-15 08:26:12 -0400

    use 1 for rate

--- script/common/Vat.sol
+++ script/common/Vat.sol
@@ -12,7 +12,7 @@ contract Vat is Ownable {
     }
 
     function ilks(bytes32) external view returns (uint256 Art, uint256 rate, uint256 spot, uint256 line, uint256 dust) {
-        return (0, 0, 0, ilkLine, 0);
+        return (0, 1e27, 0, ilkLine, 0);
     }
 
     function setIlkLine(uint256 _line) external onlyOwner {
