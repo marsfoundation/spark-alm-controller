ad4391c37aa262d3c578a757700c1b6e86a96060
Lucas Manuel <lucasmanuel.tech@gmail.com>
2024-12-06 12:30:37 -0500

    feat: Add JSONs to deployments (SC-859) (#63)
    * feat: add jsons to deployments
    
    * feat: update prod addresses
    
    * fix: update scripts and json
    
    * feat: update json names
    
    * feat: update addresses in json
    
    * fix: update addresses

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
 # Staging Deployments
-deploy-sepolia-staging  :; forge script script/staging/DeploySepolia.s.sol:DeploySepoliaStaging --sender ${ETH_FROM} --broadcast --slow
-deploy-ethereum-staging :; forge script script/staging/DeployEthereum.s.sol:DeployEthereumStaging --sender ${ETH_FROM} --broadcast --slow --verify
+deploy-mainnet-staging-full       :; ENV=staging forge script script/Deploy.s.sol:DeployMainnetFull --sender ${ETH_FROM} --broadcast --verify
+deploy-mainnet-staging-controller :; ENV=staging forge script script/Deploy.s.sol:DeployMainnetController --sender ${ETH_FROM} --broadcast --verify
+
+deploy-base-staging-full       :; CHAIN=base ENV=staging forge script script/Deploy.s.sol:DeployForeignFull --sender ${ETH_FROM} --broadcast --verify
+deploy-base-staging-controller :; CHAIN=base ENV=staging forge script script/Deploy.s.sol:DeployForeignController --sender ${ETH_FROM} --broadcast --verify
 
 # Production Deployments
-deploy-base     :; forge script script/Deploy.s.sol:DeployBaseFull --sender ${ETH_FROM} --broadcast --verify
-deploy-ethereum :; forge script script/Deploy.s.sol:DeployMainnetFull --sender ${ETH_FROM} --broadcast --verify
+deploy-mainnet-production-full       :; ENV=production forge script script/Deploy.s.sol:DeployMainnetFull --sender ${ETH_FROM} --broadcast --verify
+deploy-mainnet-production-controller :; ENV=production forge script script/Deploy.s.sol:DeployMainnetController --sender ${ETH_FROM} --broadcast --verify
+
+deploy-base-production-full       :; CHAIN=base ENV=production forge script script/Deploy.s.sol:DeployForeignFull --sender ${ETH_FROM} --broadcast --verify
+deploy-base-production-controller :; CHAIN=base ENV=production forge script script/Deploy.s.sol:DeployForeignController --sender ${ETH_FROM} --broadcast --verify
--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.0;
 
-import "forge-std/Script.sol";
+import { ScriptTools } from "dss-test/ScriptTools.sol";
 
-import { Base }     from "spark-address-registry/src/Base.sol";
-import { Ethereum } from "spark-address-registry/src/Ethereum.sol";
+import "forge-std/Script.sol";
 
 import { ControllerInstance } from "../deploy/ControllerInstance.sol";
 
@@ -12,20 +11,30 @@ import { ForeignControllerDeploy, MainnetControllerDeploy } from "../deploy/Cont
 
 contract DeployMainnetFull is Script {
 
+    using stdJson     for string;
+    using ScriptTools for string;
+
     function run() external {
+        vm.setEnv("FOUNDRY_ROOT_CHAINID",             "1");
+        vm.setEnv("FOUNDRY_EXPORTS_OVERWRITE_LATEST", "true");
+
         vm.createSelectFork(getChain("mainnet").rpcUrl);
 
         console.log("Deploying Mainnet ALMProxy, Controller and RateLimits...");
 
+        string memory fileSlug = string(abi.encodePacked("mainnet-", vm.envString("ENV")));
+
         vm.startBroadcast();
 
+        string memory config = ScriptTools.loadConfig(fileSlug);
+
         ControllerInstance memory instance = MainnetControllerDeploy.deployFull({
-            admin   : Ethereum.SPARK_PROXY,
-            vault   : Ethereum.ALLOCATOR_VAULT,
-            psm     : Ethereum.PSM,
-            daiUsds : Ethereum.DAI_USDS,
-            cctp    : Ethereum.CCTP_TOKEN_MESSENGER,
-            susds   : Ethereum.SUSDS
+            admin   : config.readAddress(".admin"),
+            vault   : config.readAddress(".allocatorVault"),
+            psm     : config.readAddress(".psm"),
+            daiUsds : config.readAddress(".daiUsds"),
+            cctp    : config.readAddress(".cctpTokenMessenger"),
+            susds   : config.readAddress(".susds")
         });
 
         vm.stopBroadcast();
@@ -33,48 +42,125 @@ contract DeployMainnetFull is Script {
         console.log("ALMProxy   deployed at", instance.almProxy);
         console.log("Controller deployed at", instance.controller);
         console.log("RateLimits deployed at", instance.rateLimits);
+
+        ScriptTools.exportContract(fileSlug, "almProxy",   instance.almProxy);
+        ScriptTools.exportContract(fileSlug, "controller", instance.controller);
+        ScriptTools.exportContract(fileSlug, "rateLimits", instance.rateLimits);
+    }
+
+}
+
+contract DeployMainnetController is Script {
+
+    using stdJson     for string;
+    using ScriptTools for string;
+
+    function run() external {
+        vm.setEnv("FOUNDRY_ROOT_CHAINID",             "1");
+        vm.setEnv("FOUNDRY_EXPORTS_OVERWRITE_LATEST", "true");
+
+        vm.createSelectFork(getChain("mainnet").rpcUrl);
+
+        console.log("Deploying Mainnet Controller...");
+
+        string memory fileSlug = string(abi.encodePacked("mainnet-", vm.envString("ENV")));
+
+        vm.startBroadcast();
+
+        string memory config = ScriptTools.loadConfig(fileSlug);
+
+        address controller = MainnetControllerDeploy.deployController({
+            admin      : config.readAddress(".admin"),
+            almProxy   : config.readAddress(".almProxy"),
+            rateLimits : config.readAddress(".rateLimits"),
+            vault      : config.readAddress(".allocatorVault"),
+            psm        : config.readAddress(".psm"),
+            daiUsds    : config.readAddress(".daiUsds"),
+            cctp       : config.readAddress(".cctpTokenMessenger"),
+            susds      : config.readAddress(".susds")
+        });
+
+        vm.stopBroadcast();
+
+        console.log("Controller deployed at", controller);
+
+        ScriptTools.exportContract(fileSlug, "controller", controller);
     }
 
 }
 
 contract DeployForeignFull is Script {
 
-    function deploy(
-        string memory remoteRpcUrl,
-        address admin,
-        address psm,
-        address usdc,
-        address cctp
-    )
-        internal
-    {
-        vm.createSelectFork(remoteRpcUrl);
+    using stdJson     for string;
+    using ScriptTools for string;
 
-        console.log("Deploying Mainnet ALMProxy, Controller and RateLimits...");
+    function run() external {
+        vm.setEnv("FOUNDRY_ROOT_CHAINID",             "1");
+        vm.setEnv("FOUNDRY_EXPORTS_OVERWRITE_LATEST", "true");
+
+        string memory chainName = vm.envString("CHAIN");
+        string memory fileSlug  = string(abi.encodePacked(chainName, "-", vm.envString("ENV")));
+        string memory config    = ScriptTools.loadConfig(fileSlug);
+
+        vm.createSelectFork(getChain(chainName).rpcUrl);
+
+        console.log(string(abi.encodePacked("Deploying ", chainName, " ALMProxy, Controller and RateLimits...")));
 
         vm.startBroadcast();
 
-        ControllerInstance memory instance
-            = ForeignControllerDeploy.deployFull(admin, psm, usdc, cctp);
+        ControllerInstance memory instance = ForeignControllerDeploy.deployFull({
+            admin : config.readAddress(".admin"),
+            psm   : config.readAddress(".psm"),
+            usdc  : config.readAddress(".usdc"),
+            cctp  : config.readAddress(".cctpTokenMessenger")
+        });
 
         vm.stopBroadcast();
 
         console.log("ALMProxy   deployed at", instance.almProxy);
         console.log("Controller deployed at", instance.controller);
         console.log("RateLimits deployed at", instance.rateLimits);
+
+        ScriptTools.exportContract(fileSlug, "almProxy",   instance.almProxy);
+        ScriptTools.exportContract(fileSlug, "controller", instance.controller);
+        ScriptTools.exportContract(fileSlug, "rateLimits", instance.rateLimits);
     }
+
 }
 
-contract DeployBaseFull is DeployForeignFull {
+contract DeployForeignController is Script {
+
+    using stdJson     for string;
+    using ScriptTools for string;
 
     function run() external {
-        deploy({
-            remoteRpcUrl : getChain("base").rpcUrl,
-            admin        : Base.SPARK_EXECUTOR,
-            psm          : Base.PSM3,
-            usdc         : Base.USDC,
-            cctp         : Base.CCTP_TOKEN_MESSENGER
+        vm.setEnv("FOUNDRY_ROOT_CHAINID",             "1");
+        vm.setEnv("FOUNDRY_EXPORTS_OVERWRITE_LATEST", "true");
+
+        string memory chainName = vm.envString("CHAIN");
+        string memory fileSlug  = string(abi.encodePacked(chainName, "-", vm.envString("ENV")));
+        string memory config    = ScriptTools.loadConfig(fileSlug);
+
+        vm.createSelectFork(getChain(chainName).rpcUrl);
+
+        console.log(string(abi.encodePacked("Deploying ", chainName, " Controller...")));
+
+        vm.startBroadcast();
+
+        address controller = ForeignControllerDeploy.deployController({
+            admin      : config.readAddress(".admin"),
+            almProxy   : config.readAddress(".almProxy"),
+            rateLimits : config.readAddress(".rateLimits"),
+            psm        : config.readAddress(".psm"),
+            usdc       : config.readAddress(".usdc"),
+            cctp       : config.readAddress(".cctpTokenMessenger")
         });
+
+        vm.stopBroadcast();
+
+        console.log("Controller deployed at", controller);
+
+        ScriptTools.exportContract(fileSlug, "controller", controller);
     }
 
 }
